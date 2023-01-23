1af0ab7cd8312513fad047d753f748c5d12fa146
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2023-01-23 21:29:10 +0100

    changed base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,8 +24,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    // public $baseURL = 'http://kweek.com.ng/';
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://kweek.com.ng/';
+    // public $baseURL = 'http://localhost:8080/'; #This is for testing purposes
 
     /**
      * --------------------------------------------------------------------------
