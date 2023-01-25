88cb19a0d3a127c9fa9e063345b686e4693ef293
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2023-01-25 15:04:37 +0100

    test

--- app/Views/dashboard/checked_in.php
+++ app/Views/dashboard/checked_in.php
@@ -6,8 +6,8 @@
      <th>Attendee ID</th>
      <th>First Name</th>
      <th>Last Name</th>
-     <th>Phone</th>
      <th>Email</th>
+     <th>Phone</th>
    </tr>
 
    <?php
@@ -16,6 +16,11 @@
     ?>
        <tr class="header_item">
          <td><?php echo $attendee['id'] ?></td>
+         <td><?php echo $attendee['attendee_ID'] ?></td>
+         <td><?php echo $attendee['first_name'] ?></td>
+         <td><?php echo $attendee['last_name'] ?></td>
+         <td><?php echo $attendee['email'] ?></td>
+         <td><?php echo $attendee['phone'] ?></td>
       </tr>
    <?php
       }
